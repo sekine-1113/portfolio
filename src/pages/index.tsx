9cee91ea4be1e33ced5a133645287424f24cee46
sekine104 <sekine104@outlook.jp>
2023-09-10 22:52:58 +0900

    feat: 誕生日表示を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Header from "@/components/base/Header/Header";
 
 export default function Home() {
+    const birthDay = new Date("2001/11/13");
+    const now = new Date();
+    const isBirthDay = now.getMonth() + 1 === birthDay.getMonth() + 1 && now.getDate() === birthDay.getDate();
     return (
         <div>
             <>
@@ -8,6 +11,7 @@ export default function Home() {
                 <meta name="description" content="アリスのページです。" />
             </>
             <h1>アリスのウェブページ</h1>
+            <div>誕生日：{isBirthDay ? "今日" : birthDay.toLocaleString("ja-JP").split(" ")[0]}</div>
         </div>
     )
 }
