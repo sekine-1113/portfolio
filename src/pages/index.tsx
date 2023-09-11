de63fab96a02b0bb92d21182041074ce8091b936
sekine104 <sekine104@outlook.jp>
2023-09-11 11:32:13 +0900

    add: プロフィール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,16 @@ export default function Home() {
                 <meta name="description" content="アリスのページです。" />
             </>
             <h1>アリスのウェブページ</h1>
+            <h2>プロフィール</h2>
+            <div>名前：アリス</div>
+            <div>性別：男</div>
+            <div>職業：大学生・Webエンジニア</div>
+            <div>年齢：{now.getFullYear() - birthDay.getFullYear() - 1 + Number(now.getMonth() + 1 >= birthDay.getMonth() + 1 && now.getDate() >= birthDay.getDate())}</div>
             <div>誕生日：{isBirthDay ? "今日" : birthDay.toLocaleString("ja-JP").split(" ")[0]}</div>
+            <div>出身地：福島県</div>
+            <div>趣味：散歩、ジョギング、お出かけ、カラオケ、ボウリング、プログラミング</div>
+            <div>好きなタイプ：僕を好きでいてくれる人</div>
+            <div></div>
         </div>
     )
 }
