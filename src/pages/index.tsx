533debf538cd6a58ac3f22faf2e9cacf149fa5fc
sekine104 <sekine104@outlook.jp>
2023-10-23 22:06:35 +0900

    feat: 少し修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/base/Header/Header";
 
+
 export default function Home() {
     const birthDay = new Date("2001-11-13");
     const now = new Date();
@@ -10,8 +11,11 @@ export default function Home() {
     }
     nextBirthDay.setMonth(10);
     nextBirthDay.setDate(13);
-    const nextBirthDayAtText = `次の誕生日まであと${Math.floor(Math.floor(nextBirthDay.getTime() - now.getTime()) / 86400000)}日！`;
     const currentAge = now.getFullYear() - birthDay.getFullYear() - 1 + Number(now.getMonth() + 1 >= birthDay.getMonth() + 1 && now.getDate() >= birthDay.getDate());
+    const birthDayText = birthDay.toLocaleString("ja-JP").split(" ")[0];
+    const isBirthDayText = isBirthDay
+        ? "今日が誕生日です！！"
+        : `次の誕生日まであと${Math.floor(Math.floor(nextBirthDay.getTime() - now.getTime()) / 86400000)}日！`;
     return (
         <>
             <Header title="アリスのウェブページ" />
@@ -22,12 +26,11 @@ export default function Home() {
                 <div>性別：男</div>
                 <div>職業：大学生・Webエンジニア</div>
                 <div>年齢：{currentAge}</div>
-                <div>誕生日：{isBirthDay ? "今日" : birthDay.toLocaleString("ja-JP").split(" ")[0]}</div>
-                {isBirthDay ? "今日が誕生日です！！" : nextBirthDayAtText}
+                <div>誕生日：{birthDayText} {isBirthDayText}</div>
                 <div>出身地：福島県</div>
                 <div>趣味：読書、散歩、ジョギング、お出かけ、カラオケ、ボウリング、プログラミング</div>
                 <div>好きなタイプ：僕を好きでいてくれる人</div>
             </div>
         </>
-    )
+    );
 }
