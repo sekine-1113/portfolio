dda5f7b840b4d8018ea63999ef2480fcfe074960
sekine104 <sekine104@outlook.jp>
2023-11-05 19:18:46 +0900

    feat: 誕生日に係るロジックの修正

--- src/pages/api/v1/profile.ts
+++ src/pages/api/v1/profile.ts
@@ -3,28 +3,26 @@ import { Profile, ProfileResponse } from '../../interface/profile';
 
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-    const birthDay = new Date("2001-11-13");
+    const birthDate = new Date("2001-11-13 0:00:00");
     const now = new Date();
-    const isBirthDay = now.getMonth() + 1 === birthDay.getMonth() + 1 && now.getDate() === birthDay.getDate();
-    const nextBirthDay = new Date();
-    if (Number(now.getMonth() + 1 >= birthDay.getMonth() + 1 && now.getDate() >= birthDay.getDate())) {
-        nextBirthDay.setFullYear(now.getFullYear() + 1);
-    }
-    nextBirthDay.setMonth(10);
-    nextBirthDay.setUTCDate(13);
-    nextBirthDay.setUTCHours(0, 0, 0, 0);
-    const currentAge = now.getFullYear() - birthDay.getFullYear() - 1 + Number(now.getMonth() + 1 >= birthDay.getMonth() + 1 && now.getDate() >= birthDay.getDate());
-    const birthDayText = birthDay.toLocaleString("ja-JP").split(" ")[0];
-    const isBirthDayText = isBirthDay
-        ? "きょう！"
-        : `あと${Math.floor(Math.floor(nextBirthDay.getTime() - now.getTime()) / 86400000)}にち！`;
+    const thisYearBirthDate = new Date(now.getFullYear(), birthDate.getMonth(), birthDate.getDate(), 0, 0, 0, 0)
+    const nextYearBirthDate = new Date(now.getFullYear()+1, birthDate.getMonth(), birthDate.getDate(), 0, 0, 0, 0)
+    const currentAge = thisYearBirthDate.getFullYear() - birthDate.getFullYear() - Number((thisYearBirthDate.getTime() - now.getTime()) > 0)
+    const isBirthDay = birthDate.getMonth() == now.getMonth() && birthDate.getDate() == now.getDate()
+    const isAfterBirthDate = thisYearBirthDate.getTime() > now.getTime()
+    const nextBirthDays = isAfterBirthDate
+        ? Math.floor((thisYearBirthDate.getTime() - now.getTime()) / 86400000) + 1
+        : Math.floor((nextYearBirthDate.getTime() - now.getTime()) / 86400000)
+    const nextBirthDateText = isBirthDay
+        ? "はっぴーばーすでぃ！"
+        : `あと${nextBirthDays}にち！`
     const profile = {
         datas: [
             {itemName: "なまえ", item: "アリス"},
             {itemName: "せいべつ", item: "おとこのこ"},
             {itemName: "ねんれい", item: currentAge},
-            {itemName: "ばーすでー", item: birthDayText},
-            {itemName: "ねくすとばーすでー", item: isBirthDayText},
+            {itemName: "ばーすでー", item: birthDate.toLocaleString("ja-JP").split(" ")[0]},
+            {itemName: "ねくすとばーすでー", item: nextBirthDateText},
             {itemName: "うまれ", item: "ふくしまけん"},
             {itemName: "すきなこと", item: "いろいろ"},
             {itemName: "すきなひと", item: "すきになったひと"},
