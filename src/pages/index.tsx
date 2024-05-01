baf6ac35bad4cdeccc12d8951ada8ebf14f3b2e3
sekine-1113 <sekine104@outlook.jp>
2024-05-01 21:57:09 +0900

    修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import useSwr from 'swr'
 import Header from "@/components/base/Header/Header";
 import { ProfileResponse } from "./interface/profile";
 
-import {Cherry_Bomb_One, Hachi_Maru_Pop, Noto_Serif_JP} from "next/font/google"
+import { Cherry_Bomb_One, Hachi_Maru_Pop, Noto_Serif_JP } from "next/font/google"
 
-const fontStyle = Cherry_Bomb_One({weight: "400", preload: false});
+const fontStyle = Cherry_Bomb_One({ weight: "400", preload: false });
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
-    const { data, error, isLoading } = useSwr<ProfileResponse>('/api/v1/profile', fetcher)
+    const { data, error, isLoading } = useSwr<ProfileResponse>('/api/v1/profile', fetcher);
 
     if (error) return <div>Failed to load profile</div>
     if (isLoading) return <div>Loading...</div>
@@ -20,18 +20,13 @@ export default function Home() {
 
     return (
         <div className='container font-mono max-w-screen flex mx-auto bg-blue-50 mb-10'>
-            <Header title="アリスのウェブページ" />
+            <Header title="アリスのポートフォリオ" />
             <body className={classNames}>
                 <div>
-                    <h1 className='text-3xl text-start m-4 font-bold'>アリスのウェブページ</h1>
-                    <h2 className='text-2xl text-center m-4 font-bold'>プロフィール</h2>
+                    <h1 className='text-3xl text-start m-4 font-bold'>アリスのポートフォリオ</h1>
+                    <h2 className='text-2xl text-center m-4 font-bold'>ポートフォリオ</h2>
                     <div className=''>
-                        {data.items.map((it) => (
-                            <div key={it.itemName} className='text-lg bg-rose-100 rounded-lg m-2 p-3'>
-                                <div className='underline underline-offset-2 decoration-dashed indent-2'>{it.itemName}</div>
-                                <div className='text-center'>{!!it.isLink ? <a href={String(it.item)}>しょうさいページへ</a> : it.item}</div>
-                            </div>
-                        ))}
+                        修正中
                     </div>
                 </div>
             </body>
